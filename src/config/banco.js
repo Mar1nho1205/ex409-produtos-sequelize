5d4abf9bd124d224fb2a071f5ea1ea2f3452b7fb
Mar1nho1205 <silvajota265@gmail.com>
2026-09-22 15:51:58 -0300

    Agora vai 3.0

--- src/config/banco.js
+++ src/config/banco.js
@@ -1,8 +1,17 @@
 const { Sequelize } = require('sequelize');
+const path = require('path');
+const fs = require('fs');
+
+const dbFolder = path.join(__dirname, '..', '..', 'banco');
+
+if (!fs.existsSync(dbFolder)) {
+  fs.mkdirSync(dbFolder, { recursive: true });
+}
 
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './banco/banco.sqlite'
+  dialect: 'sqlite',
+  storage: path.join(dbFolder, 'banco.sqlite'),
+  logging: false
 });
 
 module.exports = sequelize;
