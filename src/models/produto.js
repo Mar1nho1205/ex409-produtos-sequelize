9bbf9b7809019298e7f59fa548d8bc43cf1e4a27
Mar1nho1205 <silvajota265@gmail.com>
2026-09-22 22:52:32 -0300

    Troca de storage para :memoria: em produto.js

--- src/models/produto.js
+++ src/models/produto.js
@@ -2,7 +2,7 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "produtos.sqlite",
+  storage: ":memory:",
   logging: false,
 });
 
